e658b943035a06bb6c59002b7ee00eb0bb197cff
Luca Baldini <luca.baldini@pi.infn.it>
2018-06-21 13:56:54 +0200

    Minor.

--- snippets/spline.py
+++ snippets/spline.py
@@ -33,6 +33,9 @@ y = np.exp(3. * x) * np.sin(3. * x)
 s1 = InterpolatedUnivariateSpline(x, y, k=1)
 s3 = InterpolatedUnivariateSpline(x, y, k=3)
 
+print(s1(0.234))
+print(s1.integral(0.2, 0.8))
+
 grid = np.linspace(0., 1., 100)
 plt.figure()
 plt.plot(x, y, 'o')
